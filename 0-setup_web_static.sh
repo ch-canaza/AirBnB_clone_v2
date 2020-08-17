50805033d0989eaa2d75a34d71dc962f52ea20c0
julian <juliancano.dev@gmail.com>
2020-08-17 16:12:12 -0500

    Setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@
 apt-get update && \
 apt-get install -y nginx && \
 mkdir -p -m=755 /data/web_static/{releases/test,shared} || exit 0
-echo 'Testing 123' > /data/web_static/releases/test/index.html
+echo 'Holberton School for win!' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
 insert='\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}'
