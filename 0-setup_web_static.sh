465e55fdd90157f923638cf7f6c20cb8974fde5a
ch-canaza <ch-canaza@hotmail.com>
2020-08-17 17:32:29 -0500

    fixing redirection

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,14 +2,11 @@
 # Bash script  that sets up your web servers for the deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "simple content, to test your Nginx configuration" | sudo tee /data/web_static/releases/test/index.html
+echo "simple content, to test your Nginx configuration" | sudo tee -a /data/web_static/releases/test/index.html
 ln -sf  /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 content="location /hbnb_static/ {\nalias /data/web_static/current;\n}"
-echo "$content" | sudo tee -a /etc/nginx/sites-available/default
+echo "$content" >> /etc/nginx/sites-available/default
 sudo service nginx restart
