287bb5630e43dfd6c7a97c8ed514dea72248fe54
ch-canaza <ch-canaza@hotmail.com>
2020-08-18 10:21:23 -0500

    fixing issues on servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
-# Bash script  that sets up your web servers for the deployment of web_static
-sudo apt-get -y update
+# install ngix server
+
+sudo apt-get update
 sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "simple content, to test your Nginx configuration" | sudo tee -a /data/web_static/releases/test/index.html
-ln -sf  /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-content="location /hbnb_static/ { alias /data/web_static/current;}"
-echo "$content" | sudo tee -a /etc/nginx/sites-available/default
-sudo service nginx restart
+cat > /data/web_static/releases/test/index.html << DATA
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+    <body>
+            <div>hola</div>
+    </body>
+</html>
+DATA
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu /data
+chgrp -R ubuntu /data
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
+exit 0
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,37 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+"""update - uncompress and create simbolic link"""
 
+from fabric.api import run, put, env
+from os.path import exists
+
+env.user = 'ubuntu'
 env.hosts = ['35.231.195.75', '52.91.219.244']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
     """
-    if os.path.isfile(archive_path) is False:
+        Distributes an archive to your web servers
+    """
+    if not exists(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
+    _path = archive_path.split("/")
+    path_no_ext = _path[1].split(".")[0]
 
+    try:
+        put(archive_path, "/tmp")
+        run("sudo mkdir -p /data/web_static/releases/" + path_no_ext + "/")
+        run("sudo tar -xzf /tmp/" + path_no_ext + ".tgz" +
+            " -C /data/web_static/releases/" + path_no_ext + "/")
+        run("sudo rm /tmp/" + path_no_ext + ".tgz")
+        run("sudo mv /data/web_static/releases/" + path_no_ext +
+            "/web_static/* /data/web_static/releases/" + path_no_ext + "/")
+        run("sudo rm -rf /data/web_static/releases/" +
+            path_no_ext + "/web_static")
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/" + path_no_ext +
+            "/ /data/web_static/current")
+        return True
+
+    except Exception:
+        return False
