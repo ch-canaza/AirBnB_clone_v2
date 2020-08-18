283af7ec0dfb2152b7f55f6a01a0f0b428c67cb3
ch-canaza <ch-canaza@hotmail.com>
2020-08-17 19:37:56 -0500

    upload task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+""" Fabric script (based on the file 1-pack_web_static.py)
+"""
+from fabric.api import local, run, put, env, hosts, env
+import os.path
+from datetime import datetime
+
+env.hosts = ['35.231.195.75', '52.91.219.244']
+env.user = "ubuntu"
+
+
+def do_deploy(archive_path):
+    """ that distributes an archive to your web servers, using
+    the function do_deploy:"""
+
+    if not archive_path:
+        return False
+    file_second_arg = archive_path.split("/")[-1]
+    new_file = "/data/web_static/release/" + "{}".format(file_second_arg
+                                                         .split(".")[0])
+    current_path = "data/web_static/current"
+    try:
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}/".format(new_file))
+        run("sudo tar -zxf /tmp/{} -C {}".format(file_second_arg, new_file))
+        run("sudo rm /tmp/{}".format(file_second_arg))
+        run("sudo mv {}/web_static {}/".format(new_file))
+        run("sudo rm -rf {}".format(current_path))
+        run("sudo ln -s {} {}".format(new_file, current_path))
+
+        return True
+    except:
+        return False
