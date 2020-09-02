88dd76e52d61e6699fff7d82cf2ff8f7874e8870
ch-canaza <ch-canaza@hotmail.com>
2020-09-02 16:29:53 -0500

    uploading tasks

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' initializes a Flask web application  '''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def root_slash_hbnb():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' starts a Flask web application  '''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def root_slash_hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def root_slash_c(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run()
